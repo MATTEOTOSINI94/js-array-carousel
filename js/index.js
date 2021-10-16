d2db83e2c6d4f8db3b443d86c1715d0a1ece4bf5
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-16 18:50:18 +0200

    mod

--- js/index.js
+++ js/index.js
@@ -22,24 +22,30 @@ const text = [
     'Lorem ipsum, dolor situscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-
+// VADO A CREARE DUE SELETTORI PER SELENZIONARE I BOX NEL CUI ANDRò INSERIRE LE IMMAGGINI IL TITOLO E IL TESTO[I 3 ARRAY]
 const mainBox = document.querySelector("#main-box")
 const sideBox = document.querySelector("#side-box")
 
+// VADO A CREARE UNA VARIABILE INDICE PER POI PETER GESTIRE LA NUMERAZIONE 
 let indice = 0
 
+// VADO A CREARE UN CICLO CHE MI FACCIA GIRARE LE IMMAGINI 
 for (let x = 0; x < items.length; x++) {
+    // CREO DUE COSTANTI A CUI DARò COME INDICE ZERO PER ADESSO AFFINCHè NEL IF LE CLASSI SI ATTIVINO SOLO SU QUEL INDICE
     const itemsImg = items[x];
     const country = title[x]
+    const newsText = text[x] 
     
-
+    // CREO DUE VARIABILI PER POI ASSEGNARE LE CLASSI AI MIEI ELEMENTI 
     let activeStyle = ""
-    let activeText= ""
+    let activeText = ""
+    let activeNews = ""
 
-
+    // SE LA INDICE[X] è ZERO ATTIVA LE DUE vas
     if ( x === indice){
         activeStyle = "active"
         activeText = "active-text"
+        activeNews = "active-news"
         
     }
 
@@ -47,29 +53,36 @@ for (let x = 0; x < items.length; x++) {
     console.log(imgElement)
     const textElement= `<h1 class="${activeText}">${country}</h1>`
     console.log(textElement)
+    const newsElement=  `<p class="${activeNews}">${newsText}</p>`
 
 
     mainBox.innerHTML += imgElement 
     sideBox.innerHTML += imgElement
     mainBox.innerHTML += textElement
-    
-
+    mainBox.innerHTML += newsElement
 }
 const arrowUp = document.querySelector(".arrow-up")
 arrowUp.addEventListener("click", function(){
 
     const activeButton = document.querySelectorAll(".active")
     const activeTextStyle = document.querySelector(".active-text")
+    const activeNewsStyle = document.querySelector(".active-news")
+    
     console.log(activeButton)
     activeButton[0].classList.remove("active")
     activeButton[1].classList.remove("active")
     activeTextStyle.classList.remove("active-text")
+    activeNewsStyle.classList.remove("active-news")
+
    
     indice--
+
+
     console.log("nuovo indice",indice)
     const imgDom = document.querySelectorAll("#main-box img")
     const imgThumb =document.querySelectorAll("#side-box img")
     const styleText=document.querySelectorAll("h1")
+    const styleNews=document.querySelectorAll("p")
 
     
     if(indice < 0){
@@ -79,6 +92,7 @@ arrowUp.addEventListener("click", function(){
     imgDom[indice].classList.add("active")
     imgThumb[indice].classList.add("active")
     styleText[indice].classList.add("active-text")
+    styleNews[indice].classList.add("active-news")
 
 
 
@@ -91,17 +105,25 @@ arrowDown.addEventListener("click", function(){
 
     const activeButton = document.querySelectorAll(".active")
     const activeTextStyle = document.querySelector(".active-text")
+    const activeNewsStyle = document.querySelector(".active-news")
+    
     console.log(activeButton)
     activeButton[0].classList.remove("active")
     activeButton[1].classList.remove("active")
     activeTextStyle.classList.remove("active-text")
+    activeNewsStyle.classList.remove("active-news")
+
    
     indice++
-   
+
+
+    console.log("nuovo indice",indice)
     const imgDom = document.querySelectorAll("#main-box img")
     const imgThumb =document.querySelectorAll("#side-box img")
     const styleText=document.querySelectorAll("h1")
+    const styleNews=document.querySelectorAll("p")
 
+    
     if(indice > 4){
         indice= items.length -5
     }
@@ -109,6 +131,8 @@ arrowDown.addEventListener("click", function(){
     imgDom[indice].classList.add("active")
     imgThumb[indice].classList.add("active")
     styleText[indice].classList.add("active-text")
-   
+    styleNews[indice].classList.add("active-news")
+
+
    })
 
